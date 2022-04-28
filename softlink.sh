834edc8b13e63c5c2e659761bd8311555911ddb4
Daniel Woznicki <danwoz@nettoolkit.com>
2022-04-28 11:35:09 -0700

    Added script to create softlinks

---
+++ softlink.sh
@@ -0,0 +1,3 @@
+# NOTE: This script must be run from the dotfiles/ root to work!
+ln -nfs `pwd`/.config/starship.toml ~/.config/starship.toml
+ln -nfs `pwd`/.config/nvim/init.vim ~/.config/nvim/init.vim
