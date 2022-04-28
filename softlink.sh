09c2783f05541fc76854d6c9c1e3bd22b5a9bd4c
Daniel Woznicki <danwoz@nettoolkit.com>
2022-04-28 11:36:44 -0700

    Added bash files to aliases

--- softlink.sh
+++ softlink.sh
@@ -1,3 +1,5 @@
 # NOTE: This script must be run from the dotfiles/ root to work!
+ln -nfs `pwd`/.bashrc ~/.bashrc
+ln -nfs `pwd`/.bash_aliases ~/.bash_aliases
 ln -nfs `pwd`/.config/starship.toml ~/.config/starship.toml
 ln -nfs `pwd`/.config/nvim/init.vim ~/.config/nvim/init.vim
