04f1eda5b34b9785c44900d113584ea0d1bfb457
Daniel Woznicki <danwoz@nettoolkit.com>
2022-05-25 14:35:26 -0700

    Fixed a couple bugs in the install script

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
+# Install the following packages:
+# - ripgrep: grep alternative
+# - fd-find: find alternative
+# - xclip: access the system clipboard from the command line
+# - curl: standard network tool
+apt --version
+if [ $? -eq 0 ]; then
+    echo "Found \`apt\` binary. Installing some utilities with \`apt\`."
+    sudo apt install ripgrep fd-find xclip curl
+fi
+
 # Insall Starship.rs, which provides a nice command line prompt.
 # https://starship.rs/
 starship --version
 if [ $? -ne 0 ]; then
     curl -sS https://starship.rs/install.sh | sh
-fi
-
-# Install ripgrep, a faster, more ergonomic alternative to grep.
-# Install fd-find, a faster, more ergnonmic alternative to find.
-apt --version
-if [ $? -eq 0 ]; then
-    echo "Found \`apt\` binary. Installing some utilities with \`apt\`."
-    sudo apt install ripgrep fd-find
 fi
 
 # Install nvm, a node.js version manager.
--- softlink.sh
+++ softlink.sh
@@ -2,5 +2,7 @@
 ln -nfs `pwd`/.bashrc ~/.bashrc
 ln -nfs `pwd`/.bash_aliases ~/.bash_aliases
 ln -nfs `pwd`/.config/starship.toml ~/.config/starship.toml
+mkdir -p ~/.config/nvim/
 ln -nfs `pwd`/.config/nvim/init.vim ~/.config/nvim/init.vim
+mkdir -p ~/.local/bin/
 ln -nfs `pwd`/.local/bin/* ~/.local/bin/
