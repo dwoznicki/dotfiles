02950d0848d522ff58515cfe133b635ad10cc716
Daniel Woznicki <danwoz@nettoolkit.com>
2022-05-25 14:40:31 -0700

    Added inputrc to ignore case

--- softlink.sh
+++ softlink.sh
@@ -6,3 +6,4 @@ mkdir -p ~/.config/nvim/
 ln -nfs `pwd`/.config/nvim/init.vim ~/.config/nvim/init.vim
 mkdir -p ~/.local/bin/
 ln -nfs `pwd`/.local/bin/* ~/.local/bin/
+ln -nfs `pwd`/.inputrc ~/.inputrc
