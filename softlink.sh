03ed52bf5523a6683bb62bcb3457ce4548c8ca04
Daniel Woznicki <danwoz@nettoolkit.com>
2023-06-26 12:12:45 -0700

    Fixed path to init file.

--- softlink.sh
+++ softlink.sh
@@ -3,7 +3,7 @@ ln -nfs `pwd`/.bashrc ~/.bashrc
 ln -nfs `pwd`/.bash_aliases ~/.bash_aliases
 ln -nfs `pwd`/.config/starship.toml ~/.config/starship.toml
 mkdir -p ~/.config/nvim/
-ln -nfs `pwd`/.config/nvim/init.vim ~/.config/nvim/init.vim
+ln -nfs `pwd`/.config/nvim/init.lua ~/.config/nvim/init.lua
 mkdir -p ~/.local/bin/
 ln -nfs `pwd`/.local/bin/* ~/.local/bin/
 ln -nfs `pwd`/.inputrc ~/.inputrc
