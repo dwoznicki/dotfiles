26a0d343d0e6457f3ef104834dbc6840079e0358
Daniel Woznicki <danwoz@naxmac.local>
2024-06-16 14:56:19 -0700

    Generalized scripts for mac/linux.

--- install.sh
+++ install.sh
@@ -1,15 +1,39 @@
-# Install the following packages:
-# - ripgrep: grep alternative
-# - fd-find: find alternative
-# - xclip: access the system clipboard from the command line
-# - curl: standard network tool
-# - flameshot: a better screenshot program
-apt --version
-if [ $? -eq 0 ]; then
-    echo "Found \`apt\` binary. Installing some utilities with \`apt\`."
-    sudo apt install ripgrep fd-find xclip curl flameshot
-    echo "Installing additional dependencies."
-    sudo apt install gcc g++ build-essential
+case "`uname`" in
+    Darwin) OSNAME="macos" ;;
+    *Linux*) OSNAME="linux" ;;
+    *) echo "Unsupported OS: `uname`"; exit 1 ;;
+esac
+if [ $OSNAME == "macos" ]; then
+    brew --version
+    if [ $? -ne 0 ]; then
+        echo "Installing homebrew." 
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        if [ $? -ne 0 ]; then
+            exit 1
+        fi
+        (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+    fi
+    brew --version
+    if [ $? -ne 0 ]; then
+        echo "Unable to find homebrew." >&2
+        exit 1
+    fi
+    brew install neovim fd ripgrep
+elif [ $OSNAME == "linux" ]; then
+    # Install the following packages:
+    # - ripgrep: grep alternative
+    # - fd-find: find alternative
+    # - xclip: access the system clipboard from the command line
+    # - curl: standard network tool
+    # - flameshot: a better screenshot program
+    apt --version
+    if [ $? -eq 0 ]; then
+        echo "Found \`apt\` binary. Installing some utilities with \`apt\`."
+        sudo apt install ripgrep fd-find xclip curl flameshot
+        echo "Installing additional dependencies."
+        sudo apt install gcc g++ build-essential
+    fi
 fi
 
 # Insall Starship.rs, which provides a nice command line prompt.
@@ -25,11 +49,3 @@ nvm --version
 if [ $? -ne 0 ]; then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 fi
-
-# Install vim-plug, a vim package manager.
-# https://github.com/junegunn/vim-plug
-if [ ! -f "~/.local/share/nvim/site/autoload/plug.vim" ]; then
-    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-fi
-
--- softlink.sh
+++ softlink.sh
@@ -1,9 +1,16 @@
 # NOTE: This script must be run from the dotfiles/ root to work!
-#ln -nfs `pwd`/.bashrc ~/.bashrc
-#ln -nfs `pwd`/.bash_aliases ~/.bash_aliases
+case "`uname`" in
+    Darwin) OSNAME="macos" ;;
+    *Linux*) OSNAME="linux" ;;
+    *) echo "Unsupported OS: `uname`"; exit 1 ;;
+esac
+if [ $OSNAME == "linux" ]; then
+    ln -nfs `pwd`/.bashrc ~/.bashrc
+    ln -nfs `pwd`/.bash_aliases ~/.bash_aliases
+    ln -nfs `pwd`/.inputrc ~/.inputrc
+fi
 ln -nfs `pwd`/.config/starship.toml ~/.config/starship.toml
 mkdir -p ~/.config/nvim/
 ln -nfs `pwd`/.config/nvim/init.lua ~/.config/nvim/init.lua
 mkdir -p ~/.local/bin/
 ln -nfs `pwd`/.local/bin/* ~/.local/bin/
-#ln -nfs `pwd`/.inputrc ~/.inputrc
